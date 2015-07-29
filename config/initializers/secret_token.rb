7bf5b47590a827ee17d0c0113a8c3c6e1962d9ee
Julian <julian@JuliansMacBook.local>
2015-07-29 14:53:22 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThrillHouse::Application.config.secret_key_base = 'fcfba7dd7358b3766e9b04a656c050b616b908f6b63e104b067557425de22bb1e7698a16771e5af6b57fa6d02c00e692ec9f9da9c63584903d8ace974a4af807'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThrillHouse::Application.config.session_store :cookie_store, key: '_ThrillHouse_session'
